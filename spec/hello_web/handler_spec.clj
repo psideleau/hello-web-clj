6b1153e528add93356ed95cbf8898cc62cf33265
Paul Sideleau <paul.sideleau@surveysampling.com>
2016-02-05 00:52:06 -0500

    homework assignment - still have cleanup work todo

--- spec/hello_web/handler_spec.clj
+++ spec/hello_web/handler_spec.clj
@@ -1,17 +1,39 @@
 (ns hello-web.handler-spec
   (:require [speclj.core :refer :all]
+            [ring.util.codec :as codec]
             [ring.mock.request :refer :all]
             [hello-web.handler :refer :all]))
 
-(describe "Testing the web app"
+(defn get-request [url]
+  (app (request :get url)))
 
+(describe "Testing the web app"
   (it "GET '/' should say Hello World."
-      (let [response (app (request :get "/"))]
+      (let [response (get-request "/")]
         (should (.contains (:body response) "Hello World"))
         (should= 200 (:status response))))
 
   (it "GET '/invalid' should say Not Found"
-      (let [response (app (request :get "/invalid"))]
-        (should= 404 (:status response)))))
+      (let [response (get-request  "/invalid")]
+        (should= 404 (:status response))))
+
+  (it "GET '/users/new' should "
+      (let [response (get-request  "/users/new")]
+        (should= 200 (:status response))))
+
+  (it "GET '/users' should should user's name "
+      (let [response (get-request "/users/2?name=paul")]
+        (should= 200 (:status response))
+        (should (.contains (:body response) "paul"))))
+
+  (it "GET '/users' should prevent cross site scripting attack"
+      (let [response (get-request (str "/users/2?name=paul" (codec/url-encode "<script>alert('test');</script>")))]
+        (should= 200 (:status response))
+        (should (.contains (:body response) "paul&lt;script&gt;alert(&apos;test&apos;);&lt;/script&gt;"))))
+
+  (it "POST '/users' should save user"
+      (let [response (app (request :post "/users" {:name "paul"}))]
+        (should= 302 (:status response))
+        (should (.contains (get (:headers response) "Location") "/users")))))
 
 (run-specs)
--- src/hello_web/routes/home.clj
+++ src/hello_web/routes/home.clj
@@ -1,9 +1,30 @@
 (ns hello-web.routes.home
-  (:require [compojure.core :refer :all]
-            [hello-web.views.layout :as layout]))
+  (:require [compojure.core :refer [GET POST defroutes]]
+            [ring.util.response :as response]
+            [hello-web.views.layout :as layout]
+            [hiccup.form :as form]
+            [hiccup.util :as util]
+            [hiccup.element :as elm]))
 
 (defn home []
-  (layout/common [:h1 "Hello World!"]))
+  (layout/common [:h1 "Hello World!"](elm/link-to "users/new" "Create new user")))
+
+(defn new-user []
+  (layout/common
+    [:h1 "New User"]
+      (form/form-to [:post "/users"]
+        (form/label "name" "Enter name")
+        (form/text-field "name")
+        (form/submit-button "Submit"))))
+
+(defn create-user! [req]
+  (response/redirect (str "/users/1?name=" (get (:params req) :name))))
+
+(defn get-user [req]
+  (layout/common [:h1 (str "User: " (util/escape-html (get (:params req) :name)))]))
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (home))
+  (GET "/users/new" req (new-user))
+  (GET "/users/:id" req (get-user req))
+  (POST "/users"    req (create-user! req)))
